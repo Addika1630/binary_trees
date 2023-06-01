77e99901ee64f4e670e6aaa46eedf3f433821a38
Abdi Berhe <abdi201380@gmail.com>
2023-06-01 10:55:11 -0700

    [Update]: Output correction

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -13,7 +13,7 @@ int binary_tree_balance(const binary_tree_t *tree)
 
 	if (!tree)
 		return (0);
-	
+
 	left_height = tree->left ? (int)binary_tree_height(tree->left) : -1;
 	right_height = tree->right ? (int)binary_tree_height(tree->right) : -1;
 
