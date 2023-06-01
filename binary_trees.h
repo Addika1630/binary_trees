61ae592907c87abdcb2f8d78e83c2cee8e89635a
Addika1630 <addika1630@gmail.com>
2023-06-01 02:25:08 -0700

    Binary trees

--- binary_trees.h
+++ binary_trees.h
@@ -53,5 +53,8 @@ void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 int binary_tree_is_complete(const binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
+int binary_tree_is_bst(const binary_tree_t *tree);
+bst_t *bst_insert(bst_t **tree, int value);
+bst_t *array_to_bst(int *array, size_t size);
 
 #endif /* _BINARY_TREES_H_ */
