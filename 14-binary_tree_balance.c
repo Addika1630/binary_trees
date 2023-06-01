590405b8f6fcf1618ab3439ef5f613d497d6a69e
Abdi Berhe <abdi201380@gmail.com>
2023-06-01 10:52:54 -0700

    [Update]: Output correction

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -9,19 +9,16 @@
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int balance_factor;
-	size_t left_height, right_height;
+	int left_height, right_height;
 
 	if (!tree)
 		return (0);
+	
+	left_height = tree->left ? (int)binary_tree_height(tree->left) : -1;
+	right_height = tree->right ? (int)binary_tree_height(tree->right) : -1;
 
-	left_height = 1 + binary_tree_height(tree->left);
-	right_height = 1 + binary_tree_height(tree->right);
-	balance_factor = left_height - right_height;
-
-	return (balance_factor);
+	return (left_height - right_height);
 }
-
 
 /**
  * binary_tree_height - Measures the height of a binary tree
@@ -38,10 +35,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	if (tree->left)
-		left_height = 1 + binary_tree_height(tree->left);
-	if (tree->right)
-		right_height = 1 + binary_tree_height(tree->right);
+	left_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	right_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 
 	return (left_height > right_height ? left_height : right_height);
 }
